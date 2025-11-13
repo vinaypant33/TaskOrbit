29ec5550cd9c39594e795331b2df26f4670c2585
Vinay Pant <vinaypant24@gmail.com>
2025-11-13 10:15:23 +0000

    Skeleton Code Done

---
+++ Desktop/Python/View/dashboard.py
@@ -0,0 +1,59 @@
+import os
+import sys
+
+import tkinter as tk
+from tkinter import ttk
+import ttkbootstrap as btk
+from ttkbootstrap.scrolled import ScrolledFrame
+
+from datetime import datetime
+from datetime import time   
+from PIL import Image, ImageTk
+
+
+# Importing the classes from the main application  : 
+
+
+
+
+
+# Main application skeleton Code  : 
+main_application  = btk.Window(themename="darkly")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main_application.mainloop()
---
+++ Desktop/Python/controller.py
@@ -0,0 +1,3 @@
+from pubsub import pub
+
+
